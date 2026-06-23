e21d577d0bacfe5c84d628e7de588b43e2c354b2
xxh <3978723553@qq.com>
2026-06-23 09:28:08 +0800

    feat: WS1修改了第5行 ->

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main() {
     std::cout << "Hello World, Team Project Workspace 1!" << std::endl;
     std::cout << "这是工作空间2（队友）添加的代码！" << std::endl;
     std::cout << "我是空间1，代码拉取成功！" << std::endl;
-    std::cout << "这是工作空间1写入的终极代码！" << std::endl;
+    std::cout << "这是队友WS2抢占的第5行！" << std::endl;
     return 0;
 }//
 // Created by 39787 on 2026/6/22.
